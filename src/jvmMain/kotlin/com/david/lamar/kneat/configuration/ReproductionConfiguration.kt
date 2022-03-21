dd1dbc7da5d7e97e47bab2accddf357b2cd77ee8
Lamar, David <david.lamar@lowes.com>
2022-03-21 16:28:08 -0400

    - Added ReproductionConfiguration.kt documentation

--- src/jvmMain/kotlin/com/david/lamar/kneat/configuration/ReproductionConfiguration.kt
+++ src/jvmMain/kotlin/com/david/lamar/kneat/configuration/ReproductionConfiguration.kt
@@ -1,5 +1,14 @@
 package com.david.lamar.kneat.configuration
 
+/**
+ * Used to configure how a [com.david.lamar.kneat.evolution.reproduction.ReproductionScheme] behaves.
+ *
+ * @property elitism The number of "elite" (fittest) members of a species that will stay in the population
+ * during the next generation
+ * @property survivalThreshold The percentage of a species population to use for reproduction; the fittest members will
+ * be saved first while the others will "die out"
+ * @property minSpeciesSize The minimum number of members a species is allowed to have after a reproduction event
+ */
 data class ReproductionConfiguration(
     val elitism: Int = 0,
     val survivalThreshold: Float = .2f,
