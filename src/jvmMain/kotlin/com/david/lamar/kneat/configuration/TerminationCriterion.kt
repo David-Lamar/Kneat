63c921659c386b8482ebc259b12ceb34c8d4f08f
Lamar, David <david.lamar@lowes.com>
2022-03-21 16:28:44 -0400

    - Added TerminationCriterion.kt documentation

--- src/jvmMain/kotlin/com/david/lamar/kneat/configuration/TerminationCriterion.kt
+++ src/jvmMain/kotlin/com/david/lamar/kneat/configuration/TerminationCriterion.kt
@@ -2,7 +2,15 @@ package com.david.lamar.kneat.configuration
 
 import com.david.lamar.kneat.genome.network.Aggregation
 
+/**
+ * The criteria used to determine if a solution to the problem domain has been solved.
+ *
+ * @property fitnessAggregationFunction The aggregation function used to determine the overall fitness of a species
+ * @property fitnessThreshold The value that the overall fitness (aggregated via [fitnessAggregationFunction]) needs
+ * to be greater than or equal to in order to count as a valid solution
+ */
+//TODO: These values should be moved into the top level configuration since they're used multiple places and contribute to the pipeline itself
 data class TerminationCriterion(
-    val fitnessAggregationFunction: Aggregation = Aggregation.Mean,
+    val fitnessAggregationFunction: Aggregation = Aggregation.Mean, // TODO: This and StagnationConfiguration should be the same. Maybe move into a common place
     val fitnessThreshold: Float
 )
