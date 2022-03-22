43f80f7c3fc5b6071ce1351d9c7918921057c8be
Lamar, David <david.lamar@lowes.com>
2022-03-22 15:51:03 -0400

    - Added Node.kt documentation

--- src/jvmMain/kotlin/com/david/lamar/kneat/genome/network/Node.kt
+++ src/jvmMain/kotlin/com/david/lamar/kneat/genome/network/Node.kt
@@ -22,15 +22,35 @@ class Node(
     override val gene: NodeGene
 ) : NetworkElement<Long, NodeGene>() {
 
+    /**
+     * The coroutine scope used to execute expensive operations inside of a node
+     *
+     * TODO: Either a reporter or exception handler should be added to this scope
+     */
     private val nodeContext: CoroutineScope = CoroutineScope(
-        context = SupervisorJob() +
-                Dispatchers.IO
+        context = SupervisorJob() + Dispatchers.IO
     )
 
+    /**
+     * The mutable flow used to represent the "value" of this node
+     */
     private val _valueFlow = MutableStateFlow(0f)
+
+    /**
+     * Used to subscribe to all updates from this node. Connections and nodes act asynchronously
+     * to more closely imitate how biological neural networks behave. This has some implications to how
+     * a Kneat neural network behaves; see [Network] for more information on this.
+     */
     val valueFlow: StateFlow<Float> = _valueFlow
 
-    private suspend fun activate(values: List<Float>) {
+    /**
+     * Triggered any time a value is posted by any connection this node has subscribed to; see [registerTrigger].
+     *
+     * The value emitted into [valueFlow] will be equal to the response (managed by the [gene]) plus the [Aggregation]
+     * (managed by the [gene]) of all [values] multiplied by the Bias (managed by the [gene]) fed into the
+     * [Activation] function (managed by the [gene]).
+     */
+    private suspend fun activate(values: List<Float>) { //TODO: Should this have a withContext? need to investigate.
         val aggregation = gene.managedAttributes.getAs<AggregationAttribute>(AGGREGATION_GENE).value
         val activation = gene.managedAttributes.getAs<ActivationAttribute>(ACTIVATION_GENE).value
         val response = gene.managedAttributes.getAs<FloatAttribute>(RESPONSE_GENE).value
@@ -40,6 +60,11 @@ class Node(
         _valueFlow.emit(activation.activate(response + aggregated * bias))
     }
 
+    /**
+     * Triggers an [activate] of this node when the [trigger] posts a value. This trigger represents
+     * a flow of ALL connections coming into this node and will update any time any of the connections
+     * post a value.
+     */
     fun registerTrigger(trigger: StateFlow<List<Float>>) {
         nodeContext.launch {
             trigger.collect {
