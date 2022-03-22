f385a8f3cec136a6e574d2ec0239373a99eb6754
Lamar, David <david.lamar@lowes.com>
2022-03-22 19:02:33 -0400

    - Added minor MultiplierType.kt documentation

--- src/jvmMain/kotlin/com/david/lamar/kneat/util/MultiplierType.kt
+++ src/jvmMain/kotlin/com/david/lamar/kneat/util/MultiplierType.kt
@@ -1,5 +1,12 @@
 package com.david.lamar.kneat.util
 
+/**
+ * Used primarily in a [com.david.lamar.kneat.genome.genes.attributes.FloatAttribute], determines whether
+ * or not the initial value should be calculated via a Gausian / Normally distributed random number or
+ * a uniformly distributed random number.
+ *
+ * [GAUSSIAN] and [NORMAL] are both the same; just different nomenclature
+ */
 enum class MultiplierType {
     GAUSSIAN, NORMAL, UNIFORM
 }
