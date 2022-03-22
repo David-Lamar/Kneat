72a513a05170987b7d423a2cdd5acecf174c1541
Lamar, David <david.lamar@lowes.com>
2022-03-22 18:51:07 -0400

    - Added ExtensionFunctions.kt documentation

--- src/jvmMain/kotlin/com/david/lamar/kneat/util/ExtensionFunctions.kt
+++ src/jvmMain/kotlin/com/david/lamar/kneat/util/ExtensionFunctions.kt
@@ -7,14 +7,26 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.withContext
 import kotlin.coroutines.coroutineContext
 
+/**
+ * Removes a random item from the collection
+ */
 fun <E> MutableList<E>.removeRandom() {
     if (isNotEmpty()) remove(random())
 }
 
+/**
+ * Adds all [items] to the collection
+ */
 fun <E> MutableList<E>.addAll(vararg items: E) {
     addAll(items)
 }
 
+/**
+ * Helper function that calculates the distance between two lists of [NetworkElement]s.
+ *
+ * For more information about how distances are calculated, see the implementation of the
+ * specific [Gene] classes used.
+ */
 inline fun <reified ID, reified GENE : Gene> List<NetworkElement<ID, GENE>>.distance(
     to: List<NetworkElement<ID, GENE>>,
     weightCoefficient: Float,
@@ -33,16 +45,19 @@ inline fun <reified ID, reified GENE : Gene> List<NetworkElement<ID, GENE>>.dist
         }
     }
 
-    val disjointNodes = (size - equalCount) + (to.size - equalCount)
-    distance += disjointNodes * disjointCoefficient
+    val disjointElements = (size - equalCount) + (to.size - equalCount)
+    distance += disjointElements * disjointCoefficient
     distance /= Integer.max(size, to.size).toFloat()
 
     return distance
 }
 
 /**
+ * [Iterable.map] but asynchronous
+ *
  * Should be used when the mapping function is expensive and potentially numerous. Using this is not
- * justified in cases where the transform function is simple / quick as the overhead doesn't justify it
+ * justified in cases where the transform function is simple / quick as the overhead of creating these
+ * async jobs isn't justified for such situations.
  */
 suspend inline fun <T, R> Iterable<T>.mapParallel(crossinline transform: suspend (T) -> R) = withContext(coroutineContext) {
     val list = mutableListOf<Deferred<R>>()
@@ -53,20 +68,6 @@ suspend inline fun <T, R> Iterable<T>.mapParallel(crossinline transform: suspend
 
     list.map { it.await() }
 }
-
-//public inline fun <T> Iterable<T>.parallelSortedBy(crossinline selector: (T) -> R?): List<T> {
-//    if (this is Collection) {
-//        if (size <= 1) return this.toList()
-//        @Suppress("UNCHECKED_CAST")
-//        return (toTypedArray<Any?>() as Array<T>).apply { sortWith(comparator) }.asList()
-//    }
-//    return toMutableList().apply { sortWith(comparator) }
-//}
-
-suspend inline fun <T, R : Comparable<R>> Collection<T>.sortedParallelBy(crossinline selector: suspend (T) -> R?) = withContext(coroutineContext) {
-    if (size <= 1) return@withContext toList()
-
-    val newSelector = mutableMapOf<T, Deferred<R?>>()
 
     for (item in this@sortedParallelBy) {
         newSelector[item] = async { selector(item) }
