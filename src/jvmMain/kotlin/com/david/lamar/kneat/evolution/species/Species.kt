c8c933e1e8df450b8db1997b19577b6c751c5e8e
Lamar, David <david.lamar@lowes.com>
2022-03-21 23:04:15 -0400

    - Added Species.kt documentation

--- src/jvmMain/kotlin/com/david/lamar/kneat/evolution/species/Species.kt
+++ src/jvmMain/kotlin/com/david/lamar/kneat/evolution/species/Species.kt
@@ -4,6 +4,16 @@ import com.david.lamar.kneat.configuration.StagnationConfiguration
 import com.david.lamar.kneat.genome.Genome
 import com.david.lamar.kneat.genome.network.Aggregation
 
+/**
+ * A single "species" or group of genomes of similar genetic makeup.
+ *
+ * @property key The unique identifier of the species
+ * @property members The individuals that make up the species
+ * @property representative The single individual that acts as an example for the entire species
+ * @property createdIn The generation that this species was created in
+ * @property fitnessAggregationFunction The [Aggregation] function used to aggregate the fitness values for all individuals
+ * in the species
+ */
 class Species(
     val key: Long,
     val members: List<Genome>,
@@ -11,14 +21,35 @@ class Species(
     private val createdIn: Long,
     private val fitnessAggregationFunction: Aggregation
 ) {
+    /**
+     * The generation that this species last improved in
+     */
     private var lastImprovedIn: Long = 0
+
+    /**
+     * The history of fitness [Aggregation]s this species has seen
+     */
     private var fitnessHistory: MutableList<Float> = mutableListOf()
 
+    /**
+     * The current fitness of this species; calculated via the [fitnessAggregationFunction]
+     */
     var currentFitness: Float = Float.MIN_VALUE
         private set
 
+    /**
+     * Whether or not this species is stagnant. Stagnation is defined via the [StagnationConfiguration] and
+     * [com.david.lamar.kneat.evolution.stagnation.StagnationScheme]
+     */
     var isStagnant: Boolean = false
+        private set
 
+    /**
+     * Checks the current fitness value compared to the last known value in [fitnessHistory]. Based on the
+     * [config], if the [StagnationConfiguration.improvementThreshold] hasn't been hit in
+     * [StagnationConfiguration.maxStagnationGeneration] generations, the species will be marked as stagnant and be
+     * considered for culling.
+     */
     fun adjustStagnation(generation: Long, config: StagnationConfiguration) {
         val previousFitness = if (fitnessHistory.isEmpty()) {
             Float.MIN_VALUE
@@ -34,11 +65,19 @@ class Species(
         isStagnant = lastImprovedIn >= config.maxStagnationGeneration
     }
 
+    /**
+     * Returns all of the fitness values for all of the individuals in the species; each fitness is indexed via
+     * the [Genome.key] for the [Genome] which it's associated with
+     */
     fun getFitnessValues(generation: Long) : List<Pair<Long, Float>> {
         return members.map { it.key to it.getFitness(generation) }
     }
 
     companion object {
+        /**
+         * Immutable function to return a new species object identical to the [Species] it's applied to, except for
+         * the [representative] and [members] being the provided values.
+         */
         fun Species.update(
             representative: Genome,
             members: List<Genome>
