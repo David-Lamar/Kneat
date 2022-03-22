ad03c557ed7ac3a078e66f1f476c7ef755aa0df1
Lamar, David <david.lamar@lowes.com>
2022-03-22 11:28:51 -0400

    - Added Gene.kt documentation

--- src/jvmMain/kotlin/com/david/lamar/kneat/genome/genes/Gene.kt
+++ src/jvmMain/kotlin/com/david/lamar/kneat/genome/genes/Gene.kt
@@ -10,11 +10,30 @@ inline fun <reified T> Map<String, Attribute<out Any, out GenomeConfiguration.At
 
 abstract class Gene {
 
+    /**
+     * The attributes managed by this gene; indexed by a string name. Genes of the same type *MUST* have
+     * the same list of managed attributes; otherwise, distance checking and crossover will fail.
+     */
     abstract val managedAttributes: Map<String, Attribute<out Any, out GenomeConfiguration.AttributeConfiguration<out Any>>>
+
+    /**
+     * Calculates the "distance" between two genes; see the implementor classes for implementation
+     * specific details about how distance is calculated.
+     */
     abstract fun distance(other: Gene) : Float
+
+    /**
+     * Copies a Gene; this is mostly a helper function to "update" a gene immutably
+     */
     abstract fun copy(attributes: Map<String, Attribute<out Any, out GenomeConfiguration.AttributeConfiguration<out Any>>>) : Gene
 
-    //TODO: note that if crossed over with a non-same-type gene, it will have undefined behavior
+    /**
+     * Used during reproduction to combine two, same-type genes. A new gene is created with genes randomly
+     * selected from this Gene and [other].
+     *
+     * If two genes aren't the same type (i.e. A [ConnectionGene] and a [NodeGene]) and an attempt is made to crossover,
+     * an [IllegalStateException] will be thrown.
+     */
     fun crossover(other: Gene) : Gene {
         val attributes = mutableMapOf<String, Attribute<out Any, out GenomeConfiguration.AttributeConfiguration<out Any>>>()
         managedAttributes.forEach { (s, _) ->
